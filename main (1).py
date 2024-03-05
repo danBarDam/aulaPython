d41159210b78fbe14b36027ba8b6796d7e52f380
danBarDam <dan.bardam@gmail.com>
2024-03-05 10:52:51 -0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,17 @@
+#Criar as variáveis
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+op = input(
+    "a para SOMA; b para SUBTRAÇÃO; c para MULTIPLICAÇÃO; d para DIVISÃO: ")
+
+#Criar as condições
+if op == 'a':
+  print(' a resposta é: ', a + b)
+if op == 'b':
+  print(' a resposta é: ', a - b)
+if op == 'c':
+  print(' a resposta é: ', a * b)
+if op == 'd':
+  print(' a resposta é: ', float(a / b))  #converter para float
+
+#Atividade: Mudar o tipo de número dos resultados para número decimal.
